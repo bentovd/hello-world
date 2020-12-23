9b5e1e68bdc70e27a19a958a78f20e48496b35d0
bentovd <73180463+bentovd@users.noreply.github.com>
2020-12-23 16:29:05 +0200

    add ass 7
    I add 1 new html and update ass 6 JS and HTML

--- ass6.js
+++ ass6.js
@@ -1,2 +1,24 @@
  function big(){document.getElementById("danielle").style.width="30vw" }
  function regular(){document.getElementById("danielle").style.width="26vw" }
+
+
+
+ function createUList(users){
+    console.log(users);
+    const curr_main = document.querySelector("main");
+    for (let user of users) {
+        const section = document.createElement('section');
+        section.innerHTML=`
+        <img src="${user.avatar}" alt="Profile Picture">
+        <div>
+            <span>${user.first_name} ${user.last_name}</span>
+            <br>
+            <a href="mailto:${user.email}">Send Email</a>
+        </div>
+        `;
+        curr_main.appendChild(section);
+    }
+}
+function uList(){fetch('https://reqres.in/api/users?page=2')
+                    .then(response => response.json()).then(responseJSON => createUList(responseJSON.data))
+                    .catch(err => console.log(err));}
